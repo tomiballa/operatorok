933579fe6f3a1d0e6d04d8f5679b7832a18455a1
tomiballa <113887289+tomiballa@users.noreply.github.com>
2023-05-24 10:45:07 +0200

    operator.cs

---
+++ Operator.cs
@@ -0,0 +1,9 @@
+namespace Operatorok
+{
+    public class Expression
+    {
+        public int Operand1 { get; set; }
+        public string Operator { get; set; }
+        public int Operand2 { get; set; }
+    }
+}
